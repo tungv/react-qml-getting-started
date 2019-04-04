e3ff59acf86e2d32cff5f4aa8989eb434f121e53
Tung <tung@bodidata.com>
2019-04-04 11:14:20 +0700

    fix: remove outermost ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -4,24 +4,13 @@ import { render } from 'react-qml';
 import React from 'react';
 
 import App from './App';
-import ErrorBoundary from './ErrorBoundary';
 
 export default root => {
-  render(
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>,
-    root
-  );
+  render(<App />, root);
   if (module.hot) {
     module.hot.accept('./App', () => {
       const NextApp = require('./App').default;
-      render(
-        <ErrorBoundary>
-          <NextApp />
-        </ErrorBoundary>,
-        root
-      );
+      render(<NextApp />, root);
     });
   }
 };
